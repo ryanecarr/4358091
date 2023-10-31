3ae27cd18768774520f76caa03e50c510c210fc7
Ryan Carr <carr.ryanedward@gmail.com>
2023-10-31 18:01:10 +0000

    1 - 3

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,2 +1,32 @@
 // Write your code here
-console.log('hello');
+class Book {
+  constructor(title, author, ISBN, numCopies) {
+    this.title;
+    this.author;
+    this.ISBN;
+    this.numCopies;
+  }
+
+  get availability() {
+    return this.getAvailability();
+  }
+
+  getAvailability() {
+    if (this.numCopies === 0) {
+      return 'out of stock'
+    } else if (this.numCopies < 10) {
+      return 'low stock'
+    }
+    return 'in stock'
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold;
+  }
+
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked;
+  }
+}
+
+const HungerGames = new Book("hunger games", "suzanne collins", 123919, 6);
--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,23 @@
 // Write your code here
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  get overview() {
+    return this.getOverview();
+  }
+
+  getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was 
+    released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+  }
+}
+
+const testMovie = new Movie("title", "director", "genre", 2020, 66);
+
+console.log(testMovie.getOverview());
